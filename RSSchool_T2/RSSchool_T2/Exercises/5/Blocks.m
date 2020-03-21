27c37180b2ce31e74e31d8a47303c2efeabb8d17
iMintDiamond <i.mint.diamond@gmail.com>
2020-03-21 22:54:30 +0300

    feat: add solution for #5

--- RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
+++ RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
@@ -1,7 +1,63 @@
 #import "Blocks.h"
 
-@implementation Blocks
+@implementation Blocks {
+    NSArray* data;
+    Class type;
+}
 
+- (instancetype) init {
+    self = [super init];
+    if (self) {
+        data = nil;
+        type = nil;
+        Blocks * __weak weakSelf = self;
+        self.blockA = ^(NSArray *array) {
+            data = array;
+            [weakSelf sum];
+        };
+        self.blockB = ^(Class class) {
+            type = class;
+            [weakSelf sum];
+        };
+    }
+    
+    return self;
+}
+
+- (void) sum {
+    if ((data == nil) || (type == nil)) {
+        return;
+    }
+    
+    if (type == [NSDate class]) {
+        NSDate *result = [NSDate new];
+        for (id datum in data) {
+            if ([datum isKindOfClass:[NSDate class]]) {
+                result = ([result laterDate:datum]);
+            }
+        }
+        NSDateFormatter *dateFormatter = [NSDateFormatter new];
+        dateFormatter.dateFormat = @"dd.MM.yyyy";
+        NSString *resultString = [dateFormatter stringFromDate:result];
+        self.blockC(resultString);
+    } else if (type == [NSString class]) {
+        NSMutableString *result = [NSMutableString new];
+        for (id datum in data) {
+            if ([datum isKindOfClass:[NSString class]]) {
+                [result appendString:datum];
+            }
+        }
+        self.blockC(result);
+    } else {
+        NSNumber *result = [NSNumber new];
+        for (id datum in data) {
+            if ([datum isKindOfClass:[NSNumber class]]) {
+                result = [NSNumber numberWithInt:[result intValue] + [datum intValue]];
+            }
+        }
+        self.blockC(result);
+    }
+}
 
 @end
 
