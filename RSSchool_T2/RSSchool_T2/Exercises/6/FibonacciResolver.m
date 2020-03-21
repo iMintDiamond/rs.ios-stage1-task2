ec49fdf120c424a06f1a80f05ba21aa4ebd1c2f7
iMintDiamond <i.mint.diamond@gmail.com>
2020-03-21 18:13:24 +0300

    feat: add solution for #6

--- RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
+++ RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
@@ -2,6 +2,28 @@
 
 @implementation FibonacciResolver
 - (NSArray*)productFibonacciSequenceFor:(NSNumber*)number {
-    return @[];
+    int prod = [number intValue];
+    int fib1 = 0;
+    int fib2 = 1;
+    
+    while (fib1 * fib2 < prod) {
+        int newFib = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = newFib;
+    }
+    
+    if (fib1 * fib2 == prod) {
+        return @[
+            [[NSNumber alloc] initWithInt:fib1],
+            [[NSNumber alloc] initWithInt:fib2],
+            @1
+        ];
+    } else {
+        return @[
+            [[NSNumber alloc] initWithInt:fib1],
+            [[NSNumber alloc] initWithInt:fib2],
+            @0
+        ];
+    }
 }
 @end
