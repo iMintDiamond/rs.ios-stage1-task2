272d669ace186fea709ac9ea9b3853c4e344febe
iMintDiamond <i.mint.diamond@gmail.com>
2020-03-22 16:06:32 +0300

    feat: add solution for #3

--- RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
+++ RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
@@ -3,6 +3,22 @@
 @implementation AbbreviationMaker
 // Complete the abbreviation function below.
 - (NSString *) abbreviationFromA:(NSString *)a toB:(NSString *)b {
-    return @"Test";
+    NSMutableString *mutatedA = [NSMutableString stringWithString:[a uppercaseString]];
+    int indexB = 0;
+    int indexA = 0;
+    while (indexA < [mutatedA length]) {
+        unichar characterA = [mutatedA characterAtIndex:indexA];
+        unichar characterB = [b characterAtIndex:indexB];
+        if (characterA == characterB) {
+            indexA++;
+            if (indexB != [b length] - 1) {
+                indexB++;
+            }
+        } else {
+            [mutatedA replaceCharactersInRange:NSMakeRange(indexA, 1) withString:@""];
+        }
+    }
+    
+    return [mutatedA isEqualToString:b] ? @"YES" : @"NO";
 }
 @end
