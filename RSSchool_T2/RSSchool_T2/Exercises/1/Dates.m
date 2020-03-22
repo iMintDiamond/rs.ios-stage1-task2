2c78f42ee78b1b05913cf62fdccfed5d27885225
iMintDiamond <i.mint.diamond@gmail.com>
2020-03-22 16:53:02 +0300

    feat: add solution for #1

--- RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
+++ RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
@@ -3,7 +3,17 @@
 @implementation Dates
 
 - (NSString *)textForDay:(NSString *)day month:(NSString *)month year:(NSString *)year {
-    return @"";
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setLocale:[NSLocale localeWithLocaleIdentifier:@"ru"]];
+    
+    NSString *dateAsString = [NSString stringWithFormat:@"%@ %@ %@", day, month, year];
+    [dateFormatter setDateFormat:@"d M y"];
+    NSDate *date  = [dateFormatter dateFromString:dateAsString];
+    [dateFormatter setDateFormat:@"d MMMM, EEEE"];
+    NSString *newDate = [dateFormatter stringFromDate:date];
+
+    return (newDate == nil) ? @"Такого дня не существует" : newDate;
 }
 
 @end
